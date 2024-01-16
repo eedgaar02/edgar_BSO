97e4442b950010883b25b4a7502f5f38c3130a65
Edgar <eedgaarlopez02@gmail.com>
2024-01-16 16:29:43 +0100

    funciones cuenta atras

--- js/scripts.js
+++ js/scripts.js
@@ -6,13 +6,12 @@ document.addEventListener("DOMContentLoaded", function () {
 
     function pre_inicio() {
         // Mostrar la barra de carga durante 5 segundos antes de ejecutar la función_iniciar
-        preInicioInterval = setInterval(function () {
+        const preInicioInterval = setInterval(function () {
             const progressPercentage = ((countdownSeconds - secondsLeft) / countdownSeconds) * 100;
             progressBar.style.width = progressPercentage + "%";
 
-            if (secondsLeft <= 5) {
+            if (secondsLeft <= 20) {
                 progressBar.style.backgroundColor = "#3ABB47";
-                document.getElementById("seg").innerHTML = secondsLeft;
             }
 
             if (secondsLeft === 0) {
@@ -21,7 +20,8 @@ document.addEventListener("DOMContentLoaded", function () {
                 funcion_iniciar();
             }
 
-            document.getElementById("instrucciones").innerHTML = "El ejercicio empieza en 5 segundos";
+            document.getElementById("seg").innerHTML = secondsLeft;
+            document.getElementById("instrucciones").innerHTML = "Inhala";
             secondsLeft--;
         }, 1000);
     }
@@ -33,45 +33,94 @@ document.addEventListener("DOMContentLoaded", function () {
             const progressPercentage = ((countdownSeconds - secondsLeft) / countdownSeconds) * 100;
             progressBar.style.width = progressPercentage + "%";
 
-            if (secondsLeft <= 5) {
+            if (secondsLeft <= 20) {
                 progressBar.style.backgroundColor = "#3ABB47";
-                document.getElementById("seg").innerHTML = secondsLeft;
             }
 
             if (secondsLeft === 0) {
                 clearInterval(timerInterval);
+                // Al finalizar la función_iniciar, ejecutar la función_mantener
                 funcion_mantener();
             }
 
+            document.getElementById("seg").innerHTML = secondsLeft;
             document.getElementById("instrucciones").innerHTML = "Inhala";
             secondsLeft--;
         }, 1000);
     }
 
-    function funcion_mantener(){
+    function funcion_mantener() {
         secondsLeft = countdownSeconds;
 
-        keepInterval = setInterval(function () {
+        timerInterval = setInterval(function () {
             const progressPercentage = ((countdownSeconds - secondsLeft) / countdownSeconds) * 100;
             progressBar.style.width = progressPercentage + "%";
 
-            if (secondsLeft <= 5) {
+            if (secondsLeft <= 20) {
                 progressBar.style.backgroundColor = "#3ABB47";
-                document.getElementById("seg").innerHTML = secondsLeft;
             }
 
             if (secondsLeft === 0) {
-                clearInterval(keepInterval);
+                clearInterval(timerInterval);
+                // Al finalizar la función_mantener, ejecutar la función_exhalar
+                funcion_exhalar();
             }
 
-            document.getElementById("instrucciones").innerHTML = "Mantén";
+            document.getElementById("seg").innerHTML = secondsLeft;
+            document.getElementById("instrucciones").innerHTML = "Manteniendo";
+            secondsLeft--;
+        }, 1000);
+    }
+
+    function funcion_exhalar() {
+        secondsLeft = countdownSeconds;
+
+        timerInterval = setInterval(function () {
+            const progressPercentage = ((countdownSeconds - secondsLeft) / countdownSeconds) * 100;
+            progressBar.style.width = progressPercentage + "%";
+
+            if (secondsLeft <= 20) {
+                progressBar.style.backgroundColor = "#3ABB47";
+            }
+
+            if (secondsLeft === 0) {
+                clearInterval(timerInterval);
+                // Al finalizar la función_exhalar, ejecutar la función_mantener2
+                funcion_mantener2();
+            }
+
+            document.getElementById("seg").innerHTML = secondsLeft;
+            document.getElementById("instrucciones").innerHTML = "Exhala";
+            secondsLeft--;
+        }, 1000);
+    }
+
+    function funcion_mantener2() {
+        secondsLeft = countdownSeconds;
+
+        timerInterval = setInterval(function () {
+            const progressPercentage = ((countdownSeconds - secondsLeft) / countdownSeconds) * 100;
+            progressBar.style.width = progressPercentage + "%";
+
+            if (secondsLeft <= 20) {
+                progressBar.style.backgroundColor = "#3ABB47";
+            }
+
+            if (secondsLeft === 0) {
+                clearInterval(timerInterval);
+                // Al finalizar la función_mantener2, ejecutar la función_inicio
+                funcion_inicio();
+            }
+
+            document.getElementById("seg").innerHTML = secondsLeft;
+            document.getElementById("instrucciones").innerHTML = "Manteniendo2";
             secondsLeft--;
         }, 1000);
     }
 
     // Modificado: Función para detener la barra de carga llamada funcion_detener
     function funcion_detener() {
-        clearInterval(keepInterval);
+        clearInterval(timerInterval);
         // Puedes agregar aquí cualquier otra lógica que desees al detener la barra de carga
     }
 
